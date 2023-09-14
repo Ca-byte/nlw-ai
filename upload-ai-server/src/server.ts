e701f0be05ac67b6f36461f12348dac82951e100
Caroline Vieira <carolinesanvieira@gmail.com>
2023-09-14 19:45:49 +0200

    class 2 - api server complete

---
+++ upload-ai-server/src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-videos";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { genarationAIRoute } from "./routes/genarate-ai-completion";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify()
+
+app.register(fastifyCors, {
+	origin: '*',
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(genarationAIRoute)
+
+app.listen({
+	port: 3333, 
+}).then(()=> {
+	console.log('HTTP Server On Fire!')
+})
