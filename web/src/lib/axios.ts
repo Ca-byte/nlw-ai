9f73f6c01e426636deab8c84285cf2a7877b194e
Caroline Vieira <carolinesanvieira@gmail.com>
2023-09-18 17:34:09 +0200

    updated backend url in axios

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://upload-ai-server-dzns7afzi-ca-byte.vercel.app/',
+  baseURL: 'https://upload-ai-server-seven.vercel.app/',
   
 });
