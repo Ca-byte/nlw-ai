10665fd198b15ae71f35fb8c7c893ef17ebc5cab
Caroline Vieira <carolinesanvieira@gmail.com>
2023-09-22 19:08:15 -0300

    vercel postgres storage setup

--- upload-ai-server/src/routes/upload-videos.ts
+++ upload-ai-server/src/routes/upload-videos.ts
@@ -11,7 +11,7 @@ import pgPromise from "pg-promise";
 const pump = promisify(pipeline)
 const pgp = pgPromise();
 
-const db = pgp('postgres://Ca-byte:7MrxGPSVv1oC@ep-shy-hill-28704694.eu-central-1.aws.neon.tech/neondb');
+const db = pgp("postgres://default:2iuIPAT1dYyQ@ep-purple-glitter-18800565-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15" || "postgres://default:2iuIPAT1dYyQ@ep-purple-glitter-18800565.eu-central-1.postgres.vercel-storage.com:5432/verceldb");
 export async function uploadVideoRoute(app:FastifyInstance) {
 	app.register(fastifyMultipart,{
 		limits: {
